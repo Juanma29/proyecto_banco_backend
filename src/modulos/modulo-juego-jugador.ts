3cce3a10d1069df43efd9150b878c804e707695d
CNA06P010\Tarde <jmavilam93@gmail.com>
2022-07-25 20:06:22 +0200

    juego casi acabado

--- src/modulos/modulo-juego-jugador.ts
+++ src/modulos/modulo-juego-jugador.ts
@@ -1,4 +1,5 @@
 import axios from 'axios';
+import { Respuesta } from './../modelos/respuesta';
 import { Wrapper } from '../modelos/wrapper';
 
 interface Anfitrion {
@@ -20,7 +21,7 @@ export class ModuloJuegoJugador {
         const respuesta = await axios.get(
           `http://192.168.0.${i}:9000/anfitrion`, 
         {
-          timeout: 5
+          timeout: 10
         });
         const nombreSala = respuesta.data;
         const direccionIp = `192.168.0.${i}`;
@@ -35,6 +36,7 @@ export class ModuloJuegoJugador {
     let salaAConectar: string;
     let anfitrionEncontrado: Anfitrion;
 
+    console.log('\n-----')
     do {
       salaAConectar = await this.w.rlp.questionAsync(
         '¿Sala? (salir para volver atrás): ');
@@ -45,9 +47,46 @@ export class ModuloJuegoJugador {
 
     } while((salaAConectar !== 'salir') && (!anfitrionEncontrado));
 
+    if(salaAConectar === 'salir') {
+      return;
+    }
 
-    console.log(`Sala seleccionada ${anfitrionEncontrado.nombreSala} - ${anfitrionEncontrado.direccionIp}`);
+    console.log(`Sala seleccionada ${anfitrionEncontrado.nombreSala} - ${anfitrionEncontrado.direccionIp}`); 
+    console.log('\n-----')
+
+    // solicita el nombre del usuario
+    const nombreJugador: string = await this.w.rlp.questionAsync('Introduzca nombre de jugador: ');
+
+    if((nombreJugador.length === 0) || (nombreJugador.length > 10)) {
+      console.log('El nombre del jugador tiene que estar comprendido entre 1 y 10 caracteres');
+      return;
+    }
+
+    // conectar al anfitrión
+    const ip = anfitrionEncontrado.direccionIp;
+    const puerto = this.w.conf.juegoPuertoExpress;
+    const respuesta = await axios.get(`http://${ip}:${puerto}/entrar/${nombreJugador}`);
+    const datos: Respuesta = respuesta.data;
+
+    // conexión correcta con el anfitrión
+    if(!datos.ok) {
+      console.log(datos.msg);
+      return;
+    }
     
+    console.log('Conexión correcta con el anfitrión. Esperando inicio del juego...');
+
+    let peliculasOculto = ''
+    setInterval(async () => {
+
+      try {
+        const respuesta = await axios.get(`http://${ip}:${puerto}/pelicula`, {
+          timeout: 100
+        })
+        peliculasOculto = respuesta.data;
+        console.log(peliculasOculto);
+      } catch {}
+    }, 3000)
 
   }
 
